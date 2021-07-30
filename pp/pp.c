c218cb1e6e094f5d8ff77fe4b3465f32bd9be886
Mango0x45 <thomasvoss@live.com>
2021-07-30 13:23:45 +0200

    Use an argv0 global and better errors

--- pp/pp.c
+++ pp/pp.c
@@ -1,35 +1,30 @@
 #define _GNU_SOURCE
 #include <sys/types.h>
 
-#include <locale.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#ifdef APPEND
-#	define PROGNAME "ap"
-#else
-#	define PROGNAME "pp"
-#endif
+const char *argv0;
 
 static void
 usage(void)
 {
-	fputs(PROGNAME ": missing oprand\n", stderr);
+	fprintf(stderr, "%s: Missing oprand\n", argv0);
 	exit(EXIT_FAILURE);
 }
 
 static void
 pp(const char *text)
 {
-#ifdef APPEND
-	char end;
-#endif
 	char *line = NULL;
 	size_t n = 0;
 	ssize_t len;
 
 	while ((len = getline(&line, &n, stdin)) != -1) {
 #ifdef APPEND
+		char end;
 		if (line[--len] == '\n') {
 			line[len] = '\0';
 			end = '\n';
@@ -43,7 +38,7 @@ pp(const char *text)
 	}
 
 	if (ferror(stdin)) {
-		perror(PROGNAME);
+		fprintf(stderr, "%s: getline: %s\n", argv0, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 }
@@ -51,14 +46,12 @@ pp(const char *text)
 int
 main(int argc, char **argv)
 {
-	int i;
+	argv0 = argv[0];
 	if (argc == 1)
 		usage();
 
-	setlocale(LC_CTYPE, "");
-
 	/* Concatinate all of argv into one string */
-	for (i = 2; i < argc; i++)
+	for (int i = 2; i < argc; i++)
 		*(argv[i] - 1) = ' ';
 
 	pp(argv[1]);
